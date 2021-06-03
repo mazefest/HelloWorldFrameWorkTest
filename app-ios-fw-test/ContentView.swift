3e30872c72597c43bcc65d03e94110be424bc675
Mazefest <colbymehmen@gmail.com>
2021-06-03 13:55:22 -0500

    Initial Commit

---
+++ app-ios-fw-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app-ios-fw-test
+//
+//  Created by Colby Mehmen on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app-ios-fw-test/app_ios_fw_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_ios_fw_testApp.swift
+//  app-ios-fw-test
+//
+//  Created by Colby Mehmen on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct app_ios_fw_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
